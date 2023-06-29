6b79f66e2c62f3f2f44028ae9b48ffe506fdfedb
kdloosararian <134308829+kdloosararian@users.noreply.github.com>
2023-06-28 21:25:03 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,48 +1,53 @@
 import os
 import csv
 
-# input path to the CSV file 
-csv_path= '/Users/katieloosararian/Class_Requirements/Module 3/Starter_Code/PyBank/Resources/budget_data.csv'
+#CSV file path with related dataset
+csv_path = '/Users/katieloosararian/Class_Requirements/Module 3/Starter_Code/PyPoll/Resources/election_data.csv'
 
-#initializing the variables 
-total_months = 0 
-net_profit_losses = 0
-changes = []
-greatest_increase = ['', 0]
-greatest_decrease = ['',0] 
+# variables 
+total_votes= 0
+candidate_votes= {}
+winner= " "
+winner_votes = 0
 
-#Read on csv file
+#read csv file 
 with open(csv_path, 'r') as file:
-    csv_reader = csv.reader(file,delimiter= ',')
+    csv_reader = csv.reader(file, delimiter= ',')
     header = next(csv_reader)
 
-    #iterate the rows over
-    for row in csv_reader:
-        #counting the # of months
-        total_months +=1
+    # iteration over the rows
+    for row in csv_reader: 
+        # count total # of votes
+        total_votes += 1
 
-        #calculate the net total of profit/losses
-        net_profit_losses += int(row[1])
-
-        #calculate the changes in profit/ losses over the entire time period
-        previous_profit = int(row[1])
-
-        if total_months > 1:
-            current_change = int(row[1]) - previous_profit
-            changes.append(current_change)
+        #Candidates names
+        candidate =  row[2]
 
 
-            #find greatest invcrease in profits
-            if current_change > greatest_increase[1]:
-                greatest_increase = [row[0], current_change]
+        # add in the candidate to dict if not already presented
+        if candidate not in candidate_votes: 
+            candidate_votes[candidate] = 0
 
-          
-#calculate the average change, set variable
-average_change =  sum(changes) / len(changes)
+        # incerement the candidate vote count
+        candidate_votes[candidate] += 1
 
-# print out the results 
-print(f"Total number of months: {total_months}")
-print(f"Net total amount of Profit/Losses: ${net_profit_losses}")
-print(f"Average change in Profit/Losses: ${average_change:.2f}")
-print(f"Greatest increase in profits: {greatest_increase[0]} (${greatest_increase[1]})")
-print(f"Greatest decrease in profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
+# calculate the % of votes each candidate win and find the winner
+results = []
+for candidate, votes in candidate_votes.items():
+    percentage = (votes / total_votes) * 100
+    results.append((candidate, votes, percentage))
+
+    if votes > winner_votes:
+        winner = candidate
+        winner_votes = votes
+
+#show results
+print("Election Results")
+print("---------------------------------")
+print(f"Total Votes: {total_votes}")
+print("---------------------------------")
+for candidates, votes, percentage in results:
+    print(f"{candidates}: {percentage: .3f}% ({votes}])")
+print("---------------------------------")
+print(f"Winner: {winner}")
+print("---------------------------------")
